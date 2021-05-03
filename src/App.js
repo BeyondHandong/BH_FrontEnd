f42a9613690b1a83834c42ffebed792ccefabbd8
kongsubin <qls0919@naver.com>
2021-05-04 04:25:41 +0900

    apply components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
 import TopBar from './components/TopBar'
+import MainCheck from './components/MainCheck'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+
+  },
+  mainTitle: {
+    fontSize: 30,
+    marginBottom: theme.spacing(10)
+  },
+  mainCheck: {
+    fontSize: 30,
+    margin: theme.spacing(10)
+  },
+}));
 
 
 export default function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <TopBar>
-        
-      </TopBar>
+    <div >
+      <TopBar></TopBar>
+      {/* <MainTitle className={classes.mainTitle}></MainTitle> */}
+      <MainCheck className={classes.mainCheck}></MainCheck>
     </div>
   );
 }
