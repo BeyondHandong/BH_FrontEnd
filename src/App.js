f4799b0ea9cc186226771d4ff04cb5f22bc17b83
Jonnyboyyyy <tjwngus927@gmail.com>
2021-05-05 22:49:14 +0900

    SignIn/SignUp site added
    Usage : http://localhost:3000/signin
    Usage : http://localhost:3000/signup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import TopBar from './components/TopBar'
-import MainCheck from './components/MainCheck'
-import Search from './components/Search'
-import Table from './components/Table'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-
-  },
-  mainTitle: {
-    fontSize: 30,
-    marginBottom: theme.spacing(10)
-  },
-  mainCheck: {
-    fontSize: 30,
-    margin: theme.spacing(10)
-  },
-}));
+import { Route } from 'react-router-dom';
+import SignUp from './Pages/SignUp'
+import SignIn from './Pages/SignIn'
+import Home from './components/Home'
 
 
 export default function App() {
-  const classes = useStyles();
 
   return (
     <div >
-      <TopBar></TopBar>
-      {/* <MainTitle className={classes.mainTitle}></MainTitle> */}
-      <MainCheck className={classes.mainCheck}></MainCheck>
-      <Search></Search>
-      <Table></Table>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import TopBar from './TopBar'
+import MainCheck from './MainCheck'
+import Search from './Search'
+import Table from './Table'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+
+  },
+  mainTitle: {
+    fontSize: 30,
+    marginBottom: theme.spacing(10)
+  },
+  mainCheck: {
+    fontSize: 30,
+    margin: theme.spacing(10)
+  },
+}));
+
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <div >
+      <TopBar></TopBar>
+      {/* <MainTitle className={classes.mainTitle}></MainTitle> */}
+      <MainCheck className={classes.mainCheck}></MainCheck>
+      <Search></Search>
+      <Table></Table>
+    </div>
+  );
+}
