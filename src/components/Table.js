f90776132db7bbe94250c8bddcd1ea75915c9db3
kongsubin <qls0919@naver.com>
2021-05-11 02:29:22 +0900

    server data

--- src/components/Table.js
+++ src/components/Table.js
@@ -11,6 +11,9 @@ import {FormatBold } from '@material-ui/icons';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 
+import useAsync from '../api/useAsync';
+import * as api from '../api/post';
+
 const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: '#D5E7F2',
@@ -39,20 +42,20 @@ const StyledTableContainer = withStyles((theme) => ({
     },
   }))(TableContainer);
 
-function createData(att, name, writer, data, views, like) {
-  return { att, name, writer, data, views, like };
-}
+// function createData(att, name, writer, data, views, like) {
+//   return { att, name, writer, data, views, like };
+// }
 
-const rows = [
-  createData('공지','사이트 규칙', '게시판지기', '2019.05.04', 2400, 420),
-  createData('FAQ','비자 관련 내용', 'OIA', '2019.05.04', 370, 43),
-  createData('FAQ','교환가능한 학교들', 'OIA', '2019.05.04', 240, 60),
-  createData('5','호주 비자 신청 알아야 할 점', '남진우학부생', '2021.05.06', 670, 23),
-  createData('4','호주 숙소 구하는 법', '남진우', '2021.05.04', 490, 39),
-  createData('3','폴란드 포즈난 지리', '서주현학부생', '2020.07.04', 490, 3),
-  createData('2','포즈난 맛집', '조나단', '2019.05.04', 190, 4),
-  createData('1','한동대학교 교환학생', '수빈', '2019.05.04', 59, 9),
-];
+// const rows = [
+//   createData('공지','사이트 규칙', '게시판지기', '2019.05.04', 2400, 420),
+//   createData('FAQ','비자 관련 내용', 'OIA', '2019.05.04', 370, 43),
+//   createData('FAQ','교환가능한 학교들', 'OIA', '2019.05.04', 240, 60),
+//   createData('5','호주 비자 신청 알아야 할 점', '남진우학부생', '2021.05.06', 670, 23),
+//   createData('4','호주 숙소 구하는 법', '남진우', '2021.05.04', 490, 39),
+//   createData('3','폴란드 포즈난 지리', '서주현학부생', '2020.07.04', 490, 3),
+//   createData('2','포즈난 맛집', '조나단', '2019.05.04', 190, 4),
+//   createData('1','한동대학교 교환학생', '수빈', '2019.05.04', 59, 9),
+// ];
 
 const useStyles = makeStyles({
   table: {
@@ -60,8 +63,17 @@ const useStyles = makeStyles({
   },
 });
 
+
 export default function CustomizedTables() {
   const classes = useStyles();
+
+  const [state] = useAsync(api.getPosts, []);
+
+  const { loading, data: rows, error } = state; 
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!rows) return null;
 
   return (
     <StyledTableContainer component={Paper}>
@@ -78,15 +90,15 @@ export default function CustomizedTables() {
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell align="right">{row.att}</StyledTableCell>
+            <StyledTableRow key={row.id}>
+              <StyledTableCell align="right">{row.type}</StyledTableCell>
               <StyledTableCell align="center" component="th" scope="row">
-                <a href="#">{row.name}</a>
+                <a >{row.title}</a>
               </StyledTableCell>
-              <StyledTableCell align="right">{row.writer}</StyledTableCell>
-              <StyledTableCell align="right">{row.data}</StyledTableCell>
-              <StyledTableCell align="right">{row.views}{'  '}<VisibilityIcon /></StyledTableCell>
-              <StyledTableCell align="right">{row.like}{'  '}<FavoriteIcon /></StyledTableCell>
+              <StyledTableCell align="right">{row.writerName}</StyledTableCell>
+              <StyledTableCell align="right">{row.writeDate}</StyledTableCell>
+              <StyledTableCell align="right">{row.scrapNum}{'  '}<VisibilityIcon /></StyledTableCell>
+              <StyledTableCell align="right">{row.helpfulNum}{'  '}<FavoriteIcon /></StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
