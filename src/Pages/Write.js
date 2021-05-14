476c933ad971eda1550024ec8804b960e8086add
Jonnyboyyyy <tjwngus927@gmail.com>
2021-05-15 01:54:15 +0900

    Writing Page (Text, category)
    TextArea and Categories in Writing Page

--- src/Pages/Write.js
+++ src/Pages/Write.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import TopBar2 from '../components/TopBar2'
 import { Provider } from '../Context';
+import TextArea from '../components/TextArea'
+import {makeStyles } from "@material-ui/core/styles";
+import CheckBox from '../components/CheckBox'
+
+
+const useStyles = makeStyles((theme) => ({
+  textarea: {
+    display: 'flex',
+    maxWidth: "1200px",
+    align: "center",
+    marginTop: theme.spacing(5),
+    marginLeft: "auto",
+    marginRight: "auto",
+    border: '2px solid rgba(0, 0, 0, 0.05)',
+  },
+}));
 
 
 
-export default function Home() {
+
+export default function Write() {
+  const classes = useStyles();
 
   return (
     <Provider>
       <TopBar2></TopBar2>
       {/* <MainTitle className={classes.mainTitle}></MainTitle> */}
       {/*<MainCheck className={classes.mainCheck}></MainCheck> */}
+      
+      <div className={classes.textarea}>
+        <TextArea></TextArea>
+        </div>
+        <CheckBox></CheckBox>
     </Provider>
   );
 }
---
+++ src/components/TextArea.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { ContentState, EditorState, } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+export default class TextArea extends Component {
+
+    state = {
+        editorState: EditorState.createWithContent(ContentState.createFromText('글 내용을 작성해주세요\n\n 욕설 및 특정 타겟 비방글 금지\n 정치 및 사회 관련 글 금지\n 홍보 및 판매 글 금지'))
+    };
+
+    onEditorStateChange = (editorState) => {
+        this.setState({
+            editorState,
+        })
+    }
+
+
+
+    render() {
+        const {editorState} = this.state;
+        return (
+            <div>
+                <Editor
+                    editorState={editorState}
+                    toolbarClassName="toolbarClassName"
+                    wrapperClassName="wrapperClassName"
+                    editorClassName="demo-editor"
+                    onEditorStateChange={this.onEditorStateChange}
+                    localization={{
+                        locale: 'ko',
+                      }}
+                />
+            </div>
+        )
+    }
+}
+
